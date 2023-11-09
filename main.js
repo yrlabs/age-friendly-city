c174ac9ee7d770cd03a42fb065aea9daedbc9d4f
yrlabs <150299250+yrlabs@users.noreply.github.com>
2023-11-09 21:02:15 +0000

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function removeLoadingScreen() {
 }
 // Create a scene
 const scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xFCC467 )
+scene.background = new THREE.Color( 0xf7eab3 )
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(1, 0.5, 1);
